a26d1a5f79e7598c093a176664be1d5de789816c
HSINGH <hsingh@etsinf.upv.es>
2024-02-26 16:18:42 +0100

    Ejercicio 2

---
+++ app/src/main/java/dadm/hsingh/quotationshake/ui/about/AboutDialogFragment.kt
@@ -0,0 +1,9 @@
+package dadm.hsingh.quotationshake.ui.about
+
+import androidx.fragment.app.DialogFragment
+import dadm.hsingh.quotationshake.R
+
+
+class AboutDialogFragment: DialogFragment(R.layout.fragment_about) {
+
+}
---
+++ app/src/main/java/dadm/hsingh/quotationshake/ui/settings/SettingsFragment.kt
@@ -0,0 +1,22 @@
+package dadm.hsingh.quotationshake.ui.settings
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import dadm.hsingh.quotationshake.R
+import dadm.hsingh.quotationshake.databinding.FragmentSettingsBinding
+
+class SettingsFragment : Fragment(R.layout.fragment_settings)  {
+    private var _binding : FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentSettingsBinding.bind(view)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
