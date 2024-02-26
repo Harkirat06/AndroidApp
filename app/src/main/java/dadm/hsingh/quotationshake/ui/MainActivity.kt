3c64b3455076d1b34070355e78560af8df68fcc8
HSINGH <hsingh@etsinf.upv.es>
2024-02-26 15:45:26 +0100

    Proyecto inicial

---
+++ app/src/main/java/dadm/hsingh/quotationshake/ui/MainActivity.kt
@@ -0,0 +1,14 @@
+package dadm.hsingh.quotationshake.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dadm.hsingh.quotationshake.R
+import dadm.hsingh.quotationshake.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
