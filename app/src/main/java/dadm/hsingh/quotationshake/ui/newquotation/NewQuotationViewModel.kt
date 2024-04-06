11a81c467ed6dfa4ef97fd13f16afdad3d41849e
HSINGH <hsingh@etsinf.upv.es>
2024-04-06 17:11:25 +0200

    Ejercicio 5 terminado

--- app/src/main/java/dadm/hsingh/quotationshake/ui/newquotation/NewQuotationViewModel.kt
+++ app/src/main/java/dadm/hsingh/quotationshake/ui/newquotation/NewQuotationViewModel.kt
@@ -2,6 +2,7 @@ package dadm.hsingh.quotationshake.ui.newquotation
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import dadm.hsingh.quotationshake.data.favourites.FavouritesRepository
 import dadm.hsingh.quotationshake.data.newquotation.NewQuotationRepository
 import dadm.hsingh.quotationshake.domain.model.Quotation
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -11,6 +12,7 @@ import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 import dadm.hsingh.quotationshake.data.settings.SettingsRepository
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.stateIn
@@ -18,7 +20,8 @@ import kotlinx.coroutines.flow.stateIn
 @HiltViewModel
 class NewQuotationViewModel @Inject constructor(
     private val repository: NewQuotationRepository,
-    private val settingsRepository: SettingsRepository
+    private val settingsRepository: SettingsRepository,
+    private val favouritesRepository: FavouritesRepository
 ) : ViewModel() {
 
     val userNameFlow: StateFlow<String> = settingsRepository.getUserName().stateIn(
@@ -39,26 +42,33 @@ class NewQuotationViewModel @Inject constructor(
     private val _error = MutableStateFlow<Throwable?>(null)
     val error = _error.asStateFlow()
 
-    fun getNewQuotation() = viewModelScope.launch {
-        repository.getNewQuotation().fold(
-            onSuccess = { quotation ->
-                _quotation.value = quotation
-            },
-            onFailure = { throwable ->
-                _error.value = throwable
-            }
-        )
-        _showIcon.update {
-            false
-        }
-        _showFavouriteIcon.update {
-            true
+    fun getNewQuotation(): Job {
+        return viewModelScope.launch {
+            repository.getNewQuotation().fold(
+                onSuccess = { quotation ->
+                    _quotation.value = quotation
+                    _showIcon.update {
+                        false
+                    }
+                    _showFavouriteIcon.update {
+                        true
+                    }
+                },
+                onFailure = { throwable ->
+                    _error.value = throwable
+                }
+            )
         }
     }
 
     fun addToFavourites(){
-        _showFavouriteIcon.update {
-            false
+        viewModelScope.launch {
+            _quotation.value?.let { quotation ->
+                favouritesRepository.addQuotation(quotation)
+            }
+            _showFavouriteIcon.update {
+                false
+            }
         }
     }
 
